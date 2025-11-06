a31022bde77cce796796bff0fb126911fa49313e
khairir24983-glitch <khairir24983@masseyhigh.school.nz>
2025-11-07 11:00:18 +1300

    Getting input, attractive output

---
+++ P_O3_input_output.python.py
@@ -0,0 +1,22 @@
+# ask the user for their name
+username = input("what is your name? ")
+# ask the user for their favourite number (integer)
+
+fav_num = int(input("what is your favourite number? "))
+
+# Double, halve abd square the user's favourite number
+double = fav_num * 2
+halve = fav_num * fav_num / 2
+square = fav_num * fav_num
+
+# Greet the user
+
+print(f"\nHi {username}, your favourite number is {fav_num}")
+
+# output the results of doubling, halving and
+# squaring their favourite integer
+print(f"Double {fav_num} is {double}.")
+print (f"Half {fav_num} is {halve}")
+print(f"{fav_num} squared is {square}")
+print()
+print("Have a nice day.")
